44fc994cd6c50a9906f4a271c253a421a3bbd95d
Nozz <nozztutos@gmail.com>
2021-06-26 20:25:52 -0400

    actualizacion de datos

--- ruka_web/core/models.py
+++ ruka_web/core/models.py
@@ -12,14 +12,4 @@ class card(models.Model):
         db_table = 'card'
 
     def __repr__(self):
-        return str(self.image)
-
-#     CREATE TABLE public.card
-# (
-#     card_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-#     name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-#     series character varying(255) COLLATE pg_catalog."default" NOT NULL,
-#     value integer NOT NULL,
-#     image bytea,
-#     CONSTRAINT "PK_card" PRIMARY KEY (card_id)
-# )
+        return "name='%s', series='%s', value='%s', image='%s'" % (self.name, self.series, self.value, self.image)
