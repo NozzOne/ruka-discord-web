94ceb6041ba66a608c505de3a9cab8efa4ae008b
Nozz <nozztutos@gmail.com>
2021-08-17 21:12:34 -0400

    minor update

--- ruka_web/core/static/core/js/image_viewer.js
+++ ruka_web/core/static/core/js/image_viewer.js
@@ -40,13 +40,7 @@ modal.style.display = "none";
 function cerrar(){
 closePopup()
 };
-const copyURL = () => {
-const copyDiv = document.querySelector('.copyAlert:not(.animate)')
-if(copyDiv) {
-    copyDiv.classList.add('animate');
-    copyDiv.addEventListener('animationend', () => copyDiv.classList.remove('animate') );
-}
-};
+
 var btn = $('#button');
 
 $(window).scroll(function() {
